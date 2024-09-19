3afb46f689fd9798d0761e7fabb33577fc3f4387
AyebareGillian1 <ayebaregillian3@gmail.com>
2024-09-19 17:09:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name="AYEBARE GILLIAN";
+$DOB="19/06/2001";
+$Address="RUKIGA";
+$ZYear=2024;
+$BYear=2001;
+$Age=$ZYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address: ".$Address."<br>";
+echo"Welcome home, ".$name."<br>";
+echo"You are now , ".$Age."Years old";
+/* DOB stands for date of birth,
+ZYEAR stands for current year
+BYEAR stands for year of birth*/
+?> 
+
